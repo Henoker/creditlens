fa53ae23376878371c7df508918d2ec90e10a0a0
Henoker <hennybany@gmail.com>
2023-07-19 20:16:39 +0300

    feat: sidebard added on new page

--- creditlensapp/creditlensapp/settings.py
+++ creditlensapp/creditlensapp/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "creditlensapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': ['theme/templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
